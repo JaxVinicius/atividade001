1874e2a598a0a2fd506c668d94b3256776b772f4
JaxVinicius <jax.vinicius@gmail.com>
2020-10-21 11:15:36 -0300

    programa calculadora

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    float x, y, conta;
+    char op;
+    printf("Digite: número op número\n");
+    scanf("%f %c %f", &x, &op, &y);
+    switch(op)
+    {
+    case '*':
+        printf("O resultado da multiplicação dos dois números escolhidos é %.2f", x*y);
+    break;
+    case '-':
+        printf("O resultado da conta %.2f-%.2f é igual a %.2f", x, y, x-y);
+    break;
+    case '+':
+        printf("A soma entre os dois números escolhidos resulta em %.2f", x+y);
+    break;
+    case '/':
+        if (y!=0)
+            printf("A divisão entre %.2f e %.2f é igual a %.2f", x, y, x/y);
+        else
+            printf("Não existe divisão por 0");
+    }
+    return 0;
+}
